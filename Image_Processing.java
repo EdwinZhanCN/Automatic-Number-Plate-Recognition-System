ee1776ebef869e048a29e11e7b065dd1bac8a6b6
EdwinZhan <108310800+EdwinZhanCN@users.noreply.github.com>
2022-12-13 19:16:16 +0800

    Update Image_Processing.java

--- Image_Processing.java
+++ Image_Processing.java
@@ -7,11 +7,13 @@ import javax.imageio.ImageIO;
 public class Image_Processing{
 
     public static String processImage(String FileNameAndLocation) throws IOException {
-
+        String newFileLocation = "Photos\\Number Plate Images Processed\\" + "black_white" + FileNameAndLocation.substring(FileNameAndLocation.length()-10,FileNameAndLocation.length()-3 )+ ".jpg";
         BufferedImage img = null;
         File f = null;
 
-        // read image
+        /**
+         * Use to read the image from FileNameAndLocation.
+         */
         try {
             f = new File(FileNameAndLocation);
             img = ImageIO.read(f);
@@ -19,53 +21,46 @@ public class Image_Processing{
             System.out.println(e);
         }
 
-
+        //get the height and width of image.
         int width = img.getWidth();
         int height = img.getHeight();
-
         for (int y = 0; y < height; y++) {
-
             for (int x = 0; x < width; x++) {
-
-                // Here (x,y)denotes the coordinate of image
-                // for modifying the pixel value.
+                //get RGB.
                 int p = img.getRGB(x, y);
-
                 int a = (p >> 24) & 0xff;
                 int r = (p >> 16) & 0xff;
                 int g = (p >> 8) & 0xff;
                 int b = p & 0xff;
 
-                // calculate average
+                // calculate average to convert it into gray scale.
                 int avg = (r + g + b) / 3;
 
-                // replace RGB value with
                 p = (a << 24) | (avg << 16) | (avg << 8)
                         | avg;
-
+                //convert gray scale into binary image.
                 r = (0x00ff0000 & p) >> 16;
                 g = (0x0000ff00 & p) >> 8;
                 b = (0x000000ff & p);
                 int m = (r + g + b);
-                if (m >= 383) {
+                if (m >= 450) {
+                    //if the sum of rgb > 449, convert it into white
                     img.setRGB(x, y, Color.WHITE.getRGB());
                 } else {
+                    //else go black.
                     img.setRGB(x, y, 0);
                 }
 
             }
         }
-
-        // write image
+        // write image into new location.
         try {
-            f = new File("t.jpg");
-            FileNameAndLocation = "t.jpg";
-            ImageIO.write(img, "jpg", f);
+            File newFile = new File(newFileLocation);
+            ImageIO.write(img,"jpg",newFile);
         }
         catch (IOException e) {
             System.out.println(e);
         }
-        
-        return FileNameAndLocation;
+        return newFileLocation;
     }
 }
